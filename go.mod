a35d52394c2b69b93a6c2a2e80a684474bedf4a9
Maksym Koziar <maksym.koziar@intellecteu.com>
2021-03-30 13:23:04 +0300

    run hello world chaincode as external cc

---
+++ go.mod
@@ -0,0 +1,8 @@
+module helloworld-chaincode
+
+go 1.15
+
+require (
+	github.com/hyperledger/fabric-chaincode-go v0.0.0-20201119163726-f8ef75b17719
+	github.com/hyperledger/fabric-protos-go v0.0.0-20210310180610-ddd14b7781ba
+)
---
+++ helloworld.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	pb "github.com/hyperledger/fabric-protos-go/peer"
+	"os"
+)
+
+type HelloWorld struct {}
+
+func (h *HelloWorld) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+func (h *HelloWorld) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	function, args := stub.GetFunctionAndParameters()
+
+	if function == "invoke" {
+		return h.invoke(stub, args)
+	} else if function == "query" {
+		return h.query(stub, args)
+	}
+
+	return shim.Error("Invalid function call")
+}
+
+func (h *HelloWorld) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of args")
+	}
+
+	key := args[0]
+
+	if err := stub.PutState(key, []byte("Hello World!")); err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+func (h *HelloWorld) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of args")
+	}
+
+	key := args[0]
+
+	value, err := stub.GetState(key)
+	if err != nil {
+		return shim.Error("Error getting state: " + err.Error())
+	}
+
+	response := "{\"Greetings for " + key + "\":\"" + string(value) + "\"}"
+	return shim.Success([]byte(response))
+}
+
+func main() {
+	ccid := os.Getenv("CHAINCODE_ID")
+	address := os.Getenv("CHAINCODE_ADDRESS")
+
+	server := &shim.ChaincodeServer{
+		CCID:     ccid,
+		Address:  address,
+		CC:       &HelloWorld{},
+		TLSProps: shim.TLSProperties{
+			Disabled: true,
+		},
+	}
+
+	if err := server.Start(); err != nil {
+		fmt.Printf("Error starting HelloWorld chaincode: %s", err)
+	}
+}
